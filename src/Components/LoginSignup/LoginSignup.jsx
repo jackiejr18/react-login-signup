eca8b227ac9e19f9aad026b0fcdc152774995eee
Jackie Jr <jwysinger@gmail.com>
2024-03-19 07:30:01 -0500

    completion of html with some css

--- src/Components/LoginSignup/LoginSignup.jsx
+++ src/Components/LoginSignup/LoginSignup.jsx
@@ -28,6 +28,11 @@ const LoginSignup = () => {
           <input type='password'/>
         </div>
       </div>
+      <div className='forgot-password'>Lost Password? <span>Click Here!</span></div>
+      <div className='submit-container'>
+        <div className='submit'>Sign Up</div>
+        <div className='submit'>Login</div>
+      </div>
     </div>
   )
 }

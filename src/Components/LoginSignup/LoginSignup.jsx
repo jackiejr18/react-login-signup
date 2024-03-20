f89b36569285b9ab14f17d895cc4827b1a0f4fe2
Jackie Jr <jwysinger@gmail.com>
2024-03-20 12:41:22 -0500

    login and sign up screen hide show different div elements

--- src/Components/LoginSignup/LoginSignup.jsx
+++ src/Components/LoginSignup/LoginSignup.jsx
@@ -19,10 +19,11 @@ const LoginSignup = () => {
         <div className='underline'></div>
       </div>
       <div className='inputs'>
-        <div className='input'>
+        {action==='Login'?<div></div>: <div className='input'>
           <img src={user_icon}/>
           <input type='text' placeholder='Name'/>
-        </div>
+        </div>}
+       
         <div className='input'>
           <img src={email_icon}/>
           <input type='email' placeholder='Email Id'/>
@@ -32,7 +33,7 @@ const LoginSignup = () => {
           <input type='password' placeholder='Password'/>
         </div>
       </div>
-      <div className='forgot-password'>Lost Password? <span>Click Here!</span></div>
+      {action==='Sign Up'?<div></div>:<div className='forgot-password'>Lost Password? <span>Click Here!</span></div>}
       <div className='submit-container'>
         <div className={action==='Login'?'submit gray':'submit'} onClick={()=>{setAction('Sign Up')}}>Sign Up</div>
         <div className={action==='Sign Up'?'submit gray':'submit'} onClick={()=>{setAction('Login')}}>Login</div>
